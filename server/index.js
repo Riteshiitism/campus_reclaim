8e722bbdc049c42cc34b95bb93800601fe68a48c
Riteshiitism <120582841+Riteshiitism@users.noreply.github.com>
2023-11-30 22:44:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,10 +16,10 @@ app.use(function(req, res, next) {
   // if (allowedOrigins.includes(origin)) {
   //      res.setHeader('Access-Control-Allow-Origin', origin);
   // }
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-  next();
+  next();
 });
 
 
